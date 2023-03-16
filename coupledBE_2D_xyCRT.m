d85ebff6e956beef0260fed38e722982745cf159
hussein abduelhaleim hussein muhammed <62794321+Jaguar101-jr@users.noreply.github.com>
2023-03-17 05:23:09 +0800

    Add files via upload

--- coupledBE_2D_xyCRT.m
+++ coupledBE_2D_xyCRT.m
@@ -1,26 +1,29 @@
+
 %% This program is for solving the 2D coupled burgers' equation
-%% A Matlab code written by and developed by HUSSEIN A. H. Muhammed. March 2023.
-%% B.Sc. and M.Sc. (Honuors) degree in geophysics, 2010-2022.
+%% A Matlab code written by and developed by HUSSEIN A. H. Muhammed, March 2023.
+%% B.Sc. and M.Sc. (Honuors) degree in geophysics.
 
-%coupled BE : 
-% du/dt + u(du/dx) + v(du/dy) = v[(d^2u/dx^2)+(d^2u/dy^2)]
-% dv/dt + u(dv/dx) + v(dv/dy) = v[(d^2u/dx^2)+(d^2u/dy^2)]
+%coupled BE
+% du/dt + u(du/dx) + v(du/dy) = nu[(d^2u/dx^2)+(d^2u/dy^2)]
+% dv/dt + u(dv/dx) + v(dv/dy) = nu[(d^2u/dx^2)+(d^2u/dy^2)]
 
-clear;
+%% Prepare the movie file
+
+    vidObj = VideoWriter('coupled-BE-2d.avi');
+    open(vidObj);
 
 %% Set equation variables and parameters
 nx = 41;
 ny = 41;
 nt = 120;
-c = 0.1;
+c = 1;
 dx = 2/(nx-1);
 dy = 2/(ny-1);
-sigma = 0.009;
-nu = 0.001;
+sigma = .0009;
+nu = 0.01;
 dt = sigma*dx*dy/nu;
 
-
-%% create the computational domain
+%% create the computational grid
 x = linspace(0,2,nx);
 y = linspace(0,2,ny);
 
@@ -29,6 +32,7 @@ u= ones(ny,nx);
 v= ones(ny,nx);
 un=u;
 vn=v;
+
 comb=ones(ny,nx);
 
 %% intial conditions
@@ -36,24 +40,22 @@ comb=ones(ny,nx);
 u(0.5/dy:1/dy+1, 0.5/dx:1/dx+1) = 2;
 u(0.5/dy:1/dy+1, 0.5/dx:1/dx+1) = 2;
 
-
 [X,Y] = meshgrid(x,y);
 % surf(x,y,u)    % to test if the mesh metrices are generated
 
-%% Finite-differnce spatial & temporal discritization loop
+%% CD spatial & temporal discritization loop
 
 for n=1:nt+1
     un = u;
     vn = v;
     for i = 2:(ny-1)
         for j = 2:(nx-1)
-        u(1,j) = u(i,j) - (dt/dx) * u(i,j)*(u(i,j) - u(i-1,j)) - (dt/dy) * v(i,j) * (u(i,j)-u(i,j-1)) + (nu+dt/dx^2) * (u(i-1,j)-2*u(i,j)...
-            +u(i-1,j)+(nu*dt/dy^2)*u(i,j+1)-2*u(i,j)+u(i,j-1));
-        v(1,j) = v(i,j) - (dt/dx) * u(i,j)*(v(i,j) - v(i-1,j)) - (dt/dy) * v(i,j) * (v(i,j)-v(i,j-1)) + (nu+dt/dx^2) * (v(i-1,j)-2*v(i,j)...
-            +v(i-1,j)+(nu*dt/dy^2)*v(i,j+1)-2*v(i,j)+v(i,j-1));
+        u(1,j) = u(i,j) - (dt/dx) * u(i,j)*(u(i,j) - u(i-1,j)) - (dt/dy) * v(i,j) * (u(i,j)-u(i,j-1)) + ((nu+dt/dx^2) * (u(i-1,j)-2*u(i,j)...
+            +u(i-1,j)) + (nu*dt/dy^2)*(u(i,j+1)-2*u(i,j)+u(i,j-1)));
+        v(1,j) = v(i,j) - (dt/dx) * u(i,j)*(v(i,j) - v(i-1,j)) - (dt/dy) * v(i,j) * (v(i,j)-v(i,j-1)) + ((nu+dt/dx^2) * (v(i-1,j)-2*v(i,j)...
+            +v(i-1,j)) + (nu*dt/dy^2)*(v(i,j+1)-2*v(i,j)+v(i,j-1)));
 
         %boundary conditions
-
         u(1:ny,1)=1;
         u(1,1:nx)=1;
         u(1:nx,ny)=1;
@@ -63,16 +65,25 @@ for n=1:nt+1
         v(1:nx,ny)=1;
         v(ny, 1:nx)=1;
         end
-
     end
+    %visualise the solution
     surf(x,y,u);
-    colorbar; 
-    c = colorbar;
-    c.Label.String = 'Velocity Field (unit/sec.)';
-    set(gca, 'ydir', 'norm');
-    title('Coupled2DBurgerEquation');
+%     axis([0 Lx 0 Ly]);
+%     xlabel('horizental axis');
+%     ylabel('vertical axis');
+%     colorbar; 
+%     c = colorbar;
+%     c.Label.String = 'Velocity Field (unit/sec.)';
+%     set(gca, 'ydir', 'norm');
+%     title('Coupled_2D_BurgerEquation');
     pause(0.1);
 
+     % Write each frame to the file
+       currFrame = getframe(gcf);
+       writeVideo(vidObj,currFrame);
 
 end
+
+%% Close the file
+close(vidObj);
 
