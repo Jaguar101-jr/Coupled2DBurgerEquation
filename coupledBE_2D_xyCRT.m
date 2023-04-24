e92b4389562efcc6794db5fae72a361bfb8fea41
hussein abduelhaleim hussein muhammed <62794321+Jaguar101-jr@users.noreply.github.com>
2023-04-24 11:39:45 +0800

    Add files via upload

--- coupledBE_2D_xyCRT.m
+++ coupledBE_2D_xyCRT.m
@@ -1,18 +1,18 @@
 
 %% This program is for solving the 2D coupled burgers' equation
 %% A Matlab code written by and developed by HUSSEIN A. H. Muhammed, March 2023.
-%% B.Sc. and M.Sc. (Honuors) degree in geophysics.
+%% B.Sc. and M.Sc. (Honuors) degrees in geophysics.
 
 %coupled BE
 % du/dt + u(du/dx) + v(du/dy) = nu[(d^2u/dx^2)+(d^2u/dy^2)]
 % dv/dt + u(dv/dx) + v(dv/dy) = nu[(d^2u/dx^2)+(d^2u/dy^2)]
 
 %% Prepare the movie file
+    % 
+    % vidObj = VideoWriter('coupled-BE-2d.avi');
+    % open(vidObj);
 
-    vidObj = VideoWriter('coupled-BE-2d.avi');
-    open(vidObj);
-
-%% Set equation variables and parameters
+%% Set equation variables, computational grid and parameters
 nx = 41;
 ny = 41;
 nt = 120;
@@ -20,8 +20,8 @@ c = 1;
 dx = 2/(nx-1);
 dy = 2/(ny-1);
 sigma = .0009;
-nu = 0.01;
-dt = sigma*dx*dy/nu;
+nu = 0.01;         %%viscosity
+dt = sigma*dx*dy/nu;    %%time step
 
 %% create the computational grid
 x = linspace(0,2,nx);
@@ -44,16 +44,17 @@ u(0.5/dy:1/dy+1, 0.5/dx:1/dx+1) = 2;
 % surf(x,y,u)    % to test if the mesh metrices are generated
 
 %% CD spatial & temporal discritization loop
+% update the solution [u(i,j)] using the initial conditions [un] instead of [u] and [v]
 
 for n=1:nt+1
     un = u;
     vn = v;
     for i = 2:(ny-1)
         for j = 2:(nx-1)
-        u(1,j) = u(i,j) - (dt/dx) * u(i,j)*(u(i,j) - u(i-1,j)) - (dt/dy) * v(i,j) * (u(i,j)-u(i,j-1)) + ((nu+dt/dx^2) * (u(i-1,j)-2*u(i,j)...
-            +u(i-1,j)) + (nu*dt/dy^2)*(u(i,j+1)-2*u(i,j)+u(i,j-1)));
-        v(1,j) = v(i,j) - (dt/dx) * u(i,j)*(v(i,j) - v(i-1,j)) - (dt/dy) * v(i,j) * (v(i,j)-v(i,j-1)) + ((nu+dt/dx^2) * (v(i-1,j)-2*v(i,j)...
-            +v(i-1,j)) + (nu*dt/dy^2)*(v(i,j+1)-2*v(i,j)+v(i,j-1)));
+        u(i,j) = un(i,j) - (dt/dx) * un(i,j)*(un(i,j) - un(i-1,j)) - (dt/dy) * v(i,j) * (un(i,j)-un(i,j-1)) + ((nu*dt/dx^2) * (un(i-1,j)-2*un(i,j)...
+            +un(i-1,j)) + (nu*dt/dy^2)*(un(i,j+1)-2*un(i,j)+un(i,j-1)));
+        v(i,j) = vn(i,j) - (dt/dx) * un(i,j)*(vn(i,j) - vn(i-1,j)) - (dt/dy) * vn(i,j) * (vn(i,j)-vn(i,j-1)) + ((nu*dt/dx^2) * (vn(i-1,j)-2*vn(i,j)...
+            +vn(i-1,j)) + (nu*dt/dy^2)*(vn(i,j+1)-2*vn(i,j)+vn(i,j-1)));
 
         %boundary conditions
         u(1:ny,1)=1;
@@ -79,11 +80,11 @@ for n=1:nt+1
     pause(0.1);
 
      % Write each frame to the file
-       currFrame = getframe(gcf);
-       writeVideo(vidObj,currFrame);
+       % currFrame = getframe(gcf);
+       % writeVideo(vidObj,currFrame);
 
 end
 
-%% Close the file
-close(vidObj);
+% %% Close the file
+% close(vidObj);
 
